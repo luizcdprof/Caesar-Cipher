be881c9eb700f4463e642174cca0b10e51778b99
LuizCD Prof <luizcdprof@gmail.com>
2024-04-30 06:52:12 -0300

    Refactor

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+def create_app():
+    #Create and configure the app
+    app = Flask(__name__)
+
+    #A simple page that says Hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello World!'
+    
+    @app.route("/")
+    def home(name='Home'):
+        return render_template('index.html', name=name)
+
+    @app.route("/encode")
+    def encode(name='Encode'):
+        return render_template('encode.html', name=name)
+    
+    return app
---
+++ tests/test_encode.py
@@ -0,0 +1,6 @@
+from app import create_app
+
+flask_app = create_app()
+
+def test_encode():
+    ...
