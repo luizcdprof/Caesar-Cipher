2c985f21b5b2c983bb746c4e3ae0a649f29e30b4
LuizCD Prof <luizcdprof@gmail.com>
2024-05-16 12:27:50 -0300

    Adds form and alert

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 def create_app():
     #Create and configure the app
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'asbvjadbfvskhjdhfkjsdgsdgb'
 
-    @app.route("/")
+    @app.route('/')
     def home(name='Home'):
         return render_template('index.html', name=name)
 
-    @app.route("/encode")
+    @app.route('/encode', methods=('GET', 'POST'))
     def encode(name='Encode'):
-        return render_template('encode.html', encodedmessage="MESSAGE", name=name)
+        if request.method == 'POST':
+            cipher_key = request.form['cipher_key']
+            message = request.form['message']
+
+            if not cipher_key:
+                flash('A chave é obrigatória...')
+            elif not message:
+                flash('A mensagem é necessária...')
+            else:
+                flash("ENCODE")
+
+        return render_template('encode.html',name=name)
     
     return app
